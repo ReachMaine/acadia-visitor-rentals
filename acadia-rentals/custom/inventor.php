75719fb46c4d97ae62563313c6b108a416513b69
linda@reachmaine.com <linda@reachmaine.com>
2017-12-14 16:26:46 -0500

    hide inside view & limit social channels.

--- acadia-rentals/custom/inventor.php
+++ acadia-rentals/custom/inventor.php
@@ -8,5 +8,26 @@ function register_taxs () {
   }
 }
 
+// limit the number os social buttons
+add_filter( 'inventor_metabox_social_networks', 'custom_social_networks', 10, 2 );
 
-// add the properties metabox for
+function custom_social_networks( $social_networks, $post_type ) {
+    return array(
+        'facebook'          => 'Facebook',
+        'twitter'           => 'Twitter',
+        'instagram'         => 'Instagram',
+        'youtube'           => 'YouTube',
+    );
+}
+
+
+// disable inside view & polygon
+add_filter( 'inventor_metabox_field_enabled', 'disable_gmap_views', 10, 4 );
+function disable_gmap_views( $enabled, $metabox_id, $field_id, $post_type ) {
+    $nope = array('listing_inside_view', 'listing_map_location_polygon');
+    if ( in_array($field_id, $nope )  ) {
+        return false;
+    }
+
+    return $enabled;
+}
